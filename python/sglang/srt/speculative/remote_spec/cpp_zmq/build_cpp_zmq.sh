d3693c9ca6b280a5a153ed4e85835f7d93e4ea51
xiaoqi.31 <xiaoqi.31@jd.com>
2026-02-04 08:51:31 +0000

    fix(remote spec zmq):修复 zmq 组件偶现编译错误

--- python/sglang/srt/speculative/remote_spec/cpp_zmq/build_cpp_zmq.sh
+++ python/sglang/srt/speculative/remote_spec/cpp_zmq/build_cpp_zmq.sh
@@ -10,14 +10,6 @@ pip config set global.index-url https://mirrors.jd.com/pypi/web/simple
 pip config set global.trusted-host mirrors.jd.com
 pip install pybind11 msgpack
 
-# 清理旧 build
-rm -rf build *.so
-
 # 编译扩展
 python3 setup.py build_ext --inplace
-
-# export REMOTE_SPEC_DEBUG=1
-# 测试（可选）
-# python3 test_remote_spec_zmq.py
-
 popd
